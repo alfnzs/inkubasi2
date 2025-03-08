5d33717a5c31343320f2370d61c5fd0996945451
alfina zahra <alfinaz916@gmail.com>
2025-03-08 21:47:20 +0700

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import {initializeApp} from "firebase/app"
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD3djbHzdc1OatYqiGL4hOTCuAJ46eqNCA",
-    authDomain: "sample-app-7018c.firebaseapp.com",
-    projectId: "sample-app-7018c",
-    storageBucket: "sample-app-7018c.firebasestorage.app",
-    messagingSenderId: "940530168057",
-    appId: "1:940530168057:web:e47fc7d7f3322f756ab228"
-  };
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
 
 export const app=initializeApp(firebaseConfig);
 const db = getFirestore(app);  // Get Firestore instance
